498814e1934c5cebdde7879971562a7b46e7647a
Andrei Draganescu <me@andreidraganescu.info>
2016-01-28 15:06:56 +0200

    done?

--- app/configuration.php
+++ app/configuration.php
@@ -2,11 +2,10 @@
 namespace configuration;
 
 function load() {
-    $config = array();    
-    foreach (glob(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'configuration'.DIRECTORY_SEPARATOR.'*.php') as $filename) {
-        echo $filename;
+    $config = array(); 
+    $path = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'configuration'.DIRECTORY_SEPARATOR.'*.php';   
+    foreach (glob($path) as $filename) {
         $data = include $filename;
-        var_dump($data);
         if(is_array($data)) {
             $config = array_merge($config, $data);
         }
@@ -16,7 +15,7 @@ function load() {
 
 function connect() {
     extract(\configuration\load());
-    var_dump(\configuration\load());
+    var_dump($dsn);
     if (empty($dsn) || empty($user) || empty($password) || empty($frozen)) {
         die("Please check the configuration\database.php file");
     }
