7488d5038945bab789afde4a86fa188a3114ec37
Andrei Draganescu <me@andreidraganescu.info>
2016-01-28 15:57:32 +0200

    mini templating

--- app/web.php
+++ app/web.php
@@ -3,13 +3,38 @@ namespace web;
 
 function serve($data) {
 	\app\run('configuration', 'connect');
-    
-    render();
+    $action = \app\run('input', 'get', 'page');
+    if (empty($action)) {
+    	$action = 'index';
+    }
+    $view = view($action);
+    render($view);
 }
 
-function render() {
+function view($view) {
+	$path = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'templates';
+	$view = $path.DIRECTORY_SEPARATOR.'web'.DIRECTORY_SEPARATOR.$view.'.phtml';
+
+	if (!file_exists($view)) {
+		$view = $path.DIRECTORY_SEPARATOR.'web'.DIRECTORY_SEPARATOR.'404.phtml';
+	} else {
+		$function = $view.'_page';
+		extract($function());
+	}
+
+	ob_start();
+	include $view;
+	return ob_get_clean();
+}
+
+function render($view) {
 	$path = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'templates';
 	$layout = $path.DIRECTORY_SEPARATOR.'web'.DIRECTORY_SEPARATOR.'layout.phtml';
-	
 	include $layout;
+}
+
+function index_page() {
+	return array(
+		'today' = date("Y-m-d");
+	);
 }
